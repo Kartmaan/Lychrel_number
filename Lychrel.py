3d0456f4a2dafc3646876e1127b3cd82b0c19d8e
Kartmaan <cartmanboss69@gmail.com>
2020-11-10 14:38:03 +0100

    first commit

---
+++ Lychrel.py
@@ -0,0 +1,88 @@
+import time
+
+def pal(x) : # <int>
+    """ Return the palindrome of a int number x. """
+
+    x = int(x)
+    num = x
+    m = 0
+
+    while num > 0 :
+        m = m*10 + num%10
+        m = int(m)
+        num = num // 10
+        num = int(num)  
+    return m # <int>
+
+def ispal(x) : # <int>
+    """Palindrome detector.
+    Return True if it's a palindrome, False otherwise."""
+
+    x = int(x)
+    num = x
+    m = 0
+
+    while num > 0 :
+        m = m*10 + num%10
+        m = int(m)
+        num = num // 10
+        num = int(num)
+    
+    if m == x :
+        return True # <bool>
+    else :
+        return False # <bool>
+
+def lychrel(x, detailed = False) : # <int>
+	""" Function carrying out the sequences of operations 
+	leading to the palindrome.
+	- The intermediate stages of the operation are inserted in a 
+	list until the palindrome is obtained. 
+    - By default, only the result of intermediate operations is 
+    added to the list, but a more detailed display of the 
+    calculations is possible by setting 'detailed = True', 
+    in this case, each addition and its result appear in tulps.
+    - A duration in seconds is allocated to the calculation, 
+    beyond this allotted time the process stops, meaning that 
+    the number inserted is potentially a Lychrel number"""
+
+	x = int(x)
+	nbr = x # x = initial value, nbr = value to handle
+	wd = 5 # Time allowed for calculation (sec)
+	L = []
+	start_time = time.time()
+   
+	while True :
+		res = nbr + pal(nbr)
+		
+		# Time control (potential Lychrel number)
+		exe_time = time.time()
+		if exe_time >= start_time + wd :
+			mess = "ITER. STOP - "+str(x)+" LYCHREL NUMBER ?"
+			L = [mess]
+			break
+
+		# It's a palindrome
+		if ispal(res) == True :
+			if detailed == True:
+				L.append((nbr, pal(nbr), res))
+			else:
+				L.append(res)
+			break
+
+		# NOT a palindrome
+		elif ispal(res) == False :
+			if detailed == True:
+				L.append((nbr, pal(nbr), res))
+			else:
+				L.append(res)
+                
+			nbr = res
+			continue
+
+	return L # <list>
+
+if __name__ == "__main__" :
+    print("97 : ", lychrel(97))
+    print("154 : ", lychrel(154, detailed=True))
+    print("196 : ", lychrel(196)) # Potential Lychrel number
